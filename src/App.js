161998e628d1b8b81e5fcf44ae0e157e31577663
jane_conradie <jconradie1998@gmail.com>
2022-03-09 12:10:54 +0200

    Add: basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+//implement redux store
+//save to redux store
+//display list from store
+
 function App() {
+
+  const [fact, setFact] = useState();
+  const [favourited, setFavourited] = useState();
+  const [favouriteDisplay, setFavouriteDisplay] = useState(false);
+
+  useEffect(() => {
+    getMeowFact()
+  }, [])
+
+  function getFavourited(){
+    var favs = localStorage.getItem("Meowfacts");
+    setFavourited(favs);
+  }
+
+  function getMeowFact(){
+    fetch('https://meowfacts.herokuapp.com/').then(data => data.json()).then(responseData => {
+      setFact(responseData.data);
+    })
+  }
+
+  function favourite(){
+    var savedMeows;
+    
+    if (localStorage.getItem("Meowfacts") !== null){
+      savedMeows = JSON.parse(localStorage.getItem("Meowfacts"));
+    }
+   
+    if (savedMeows === undefined){
+      savedMeows = fact;
+    } else {
+      savedMeows.push(fact[0]);
+    }
+
+    localStorage.setItem('Meowfacts', JSON.stringify(savedMeows));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script src="https://kit.fontawesome.com/aaffc30d60.js" crossOrigin="anonymous"></script>
+      <p>{fact}</p>
+      <button onClick={() => {favourite(); getFavourited()}}><i className="fa-solid fa-heart"></i>Purrr</button>
+      <button onClick={() => getMeowFact()}>Meow</button>
+      <button onClick={() => {setFavouriteDisplay(true); getFavourited()}}>Paw</button>
+      {favouriteDisplay && (
+        <span className='favourites'>
+          <h2>Favourited</h2>
+          {
+            JSON.parse(favourited).map((fav) => {
+              
+              return (
+                <p>{fav}</p>
+              )
+            })
+          }
+        </span>
+      )}
+      
     </div>
   );
 }
